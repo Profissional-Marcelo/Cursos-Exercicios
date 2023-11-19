c40e79468de703ba6b06a091d6a7aeb9df317d61
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-11-19 12:59:33 -0300

    aula 21

---
+++ Guanabara/ExerciciosPHP/ex003/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+    
+    <h1>Testes de tipos primitivos</h1>
+    <?php
+    //  0x = hexadecimal 0b = binário 0 = octal 
+    //     $num = 0x1A;
+    //     echo "O valor da variável é $num"
+
+    // $v = "Gustavo";
+    // var_dump($v);
+
+    // // ou integer)
+    // $num = (int) 3e2; // 3 x 10 (2) COERÇÃO
+    // echo "O valor é $num ";
+    // var_dump($num)
+
+    $vet = [6, 2, 9, 3, 5];
+    var_dump($vet);
+
+    class Pessoa {
+        private string $nome;
+    }
+
+    $p = new Pessoa;
+    var_dump($p);
+
+    ?>
+</body>
+
+</html>
