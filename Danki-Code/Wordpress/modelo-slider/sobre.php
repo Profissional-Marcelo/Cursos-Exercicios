0c4d04bcf15eb5716ae14cb527b6e2a5cc2af854
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-11-20 23:21:34 -0300

    atualizando

---
+++ Danki-Code/Wordpress/modelo-slider/sobre.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Danki Code - Tema</title>
+</head>
+
+<body>
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo">
+                    <img src="img/logo.png" alt="">
+                </div>
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+            <div class="menu-mobile">
+                <span class="material-symbols-outlined"> format_align_right </span>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <div class="clear"></div>         
+
+            
+        </div>
+    </section>
+
+    <section class="sobre-equipe">
+        <div class="center">
+            <div class="w50 equipe-sobre-texto">
+                <h2>Uma equipe estratégica para um trabalho incrível</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores vero provident harum quod, debitis illo ipsa fugiat qui error quo nihil ut ipsum tenetur quia repellendus! Iste fugiat harum facilis. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="w50">
+                <img src="img/quem-somos/foto-equipe.png" alt="foto-equipe">
+            </div>
+            <div class="clear"></div>
+        </div>
+    </sction>
+
+    <section class="sobre-imagens">
+        <div class="center">
+            <div class="img-sobre">
+                <img src="img/quem-somos/imagem-sobre.png" alt="">
+            </div>
+            <div class="img-sobre">
+                <img src="img/quem-somos/imagem-sobre.png" alt="">
+            </div>
+            <div class="img-sobre">
+                <img src="img/quem-somos/imagem-sobre.png" alt="">
+            </div>
+            <div class="img-sobre">
+                <img src="img/quem-somos/imagem-sobre.png" alt="">
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <section class="metodologia">
+        <div class="center">
+            <h2>Conheça nossa metodologia</h2>
+            <p>O que acha de fazermos o que mais gostamos de fazer? Conversar <br> entre em contato por e-mail ou telefone.</p>
+            <a href="/contato">Entrar contato</a>
+        </div>
+
+      </section>
+        <div class="center">
+             <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="#">Contato</a>
+                <a href="#">FAQ</a>
+             </div>
+             <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="#">Contato</a>
+                <a href="#">FAQ</a>
+             </div>
+             <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="#">Contato</a>
+                <a href="#">FAQ</a>
+             </div>
+
+             <div style="width: 40%;text-align: right;" class="col-footer">
+                <img src="img/logo-footer.png" alt="">
+             </div>
+        </div>
+    </footer>
+</body>
+
+</html>
