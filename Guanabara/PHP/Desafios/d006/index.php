7596e31d5735057aac9100d1cd7fa433ef350a1f
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-09 20:02:05 -0300

    começando exercicio php

---
+++ Guanabara/PHP/Desafios/d006/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Desafio de PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Anatomia de uma Divisão</h1>
+        <form action="" method="get">
+            <label for="d1">Dividendo</label>
+            <input type="number" name="d1" id="d1">
+        </form>
+    </main>
+    <section>
+        <h2>Estrutura da Divisão</h2>
+    </section>
+</body>
+</html>
