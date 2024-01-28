e20ed968380271994bf837c0d801503a35b4ffcd
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-28 15:27:11 -0300

    penultimo projeto do curso de js

---
+++ Danki-Code/JS/JavaScript para jogos/projeto3 - kodi memory/app.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Carregamento dos cards
+    const cardArray = [
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'ganhou',
+            img: 'images/ganhou.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'direita',
+            img: 'images/direita.png'
+        },
+        {
+            name: 'tras',
+            img: 'images/tras.png'
+        },
+        {
+            name: 'tras',
+            img: 'images/tras.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'correndo',
+            img: 'images/correndo.png'
+        },
+        {
+            name: 'pulo',
+            img: 'images/pulo.png'
+        },
+        {
+            name: 'pulo',
+            img: 'images/pulo.png'
+        },
+        {
+            name: 'esquerda',
+            img: 'images/esquerda.png'
+        },
+        {
+            name: 'esquerda',
+            img: 'images/esquerda.png'
+        }
+    ]
+
+    const grid = document.querySelector('.grid')
+    // criando a tela
+    function createBoard(){
+        for(let i = 0; i < cardArray.length; i++){
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/card.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard()
+})
