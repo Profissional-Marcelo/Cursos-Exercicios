c1fb51351a448bc4b78f5945241dfd501ca1b1e6
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-29 01:09:02 -0300

    terminando penultimo projeto do curso de js para jogos

--- Danki-Code/JS/JavaScript para jogos/projeto3 - kodi memory/app.js
+++ Danki-Code/JS/JavaScript para jogos/projeto3 - kodi memory/app.js
@@ -51,7 +51,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     ]
 
+    cardArray.sort(() => 0.5 - Math.random())
+
     const grid = document.querySelector('.grid')
+
+
+    const resultDisplay = document.querySelector('#result')
+
+    var cardsChosen = []
+    var cardsChosenId = []
+    var pares = []
+
     // criando a tela
     function createBoard(){
         for(let i = 0; i < cardArray.length; i++){
@@ -60,6 +70,53 @@ document.addEventListener("DOMContentLoaded", () => {
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipCard)
             grid.appendChild(card)
+        }
+    }
+
+    // Conferindo pares
+    function checkforMatch(){
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        // Clicar duas vezes no mesmo card
+        if(optionOneId == optionTwoId){
+            cards[optionOneId].setAttribute('src', 'images/card.png')
+            cards[optionTwoId].setAttribute('src', 'images/card.png')
+            alert('mesmo card')
+        } 
+        // formando um par
+        else if(cardsChosen[0] == cardsChosen[1]){
+            alert("Você conseguiu um par!")
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListenner('click', flipcard)
+            pares.push(cardsChosen)
+        }
+        else {
+            cards[optionOneId].setAttribute('src', 'images/card.png')
+            cards[optionTwoId].setAttribute('src', 'images/card.png')
+            alert('tente novamente :)')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = pares.length
+
+        if(pares.length == cardArray.length/2){
+            resultDisplay.textContent = 'Parabéns, voce venceu encontrou todos os pares!'
+        }
+    }
+
+
+    // virar o card
+    function flipCard(){
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length == 2){
+            setTimeout(checkForMatch, 500)
         }
     }
 
