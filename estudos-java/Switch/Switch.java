64c6a965cef70c5dca550bd80688b2b02ed86f52
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-05-01 12:41:13 -0300

    switch

---
+++ estudos-java/Switch/Switch.java
@@ -0,0 +1,35 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);            
+
+        System.out.println("Quantas pernas?");
+        int pernas = ss.nextInt();
+
+        switch(pernas){
+            case 1:
+                System.out.println("Saci");
+                break;
+            case 2:
+                System.out.println("Bi-pedal");
+                break;
+            case 3:
+                System.out.println("Tricipal");
+                break;
+            case 4:
+                System.out.println("Quadrupedal");
+                break;
+            case 6, 8:
+                System.out.println("Aeronavegável");
+                break;
+            default:
+                System.out.println("Invalido");
+        }
+
+
+        
+    }
+}
