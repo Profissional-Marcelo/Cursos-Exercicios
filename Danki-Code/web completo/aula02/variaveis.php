f51ce280cf8401166d323fdb29e92a6f5626f672
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-11-26 12:38:42 -0300

    .

---
+++ Danki-Code/web completo/aula02/variaveis.php
@@ -0,0 +1,12 @@
+<?php 
+    echo'<pre>';
+    echo $_SERVER['SERVER_NAME'];
+    echo'</pre>';
+    // server é um array que trás diversas váriaveis do servidor dentro dele, por exemplo a SERVER_NAME, que mostra o caminho atual que estamos
+    echo'<pre>';
+    print_r($_SERVER);
+    echo'</pre>';
+    // a print_r, mostra todas as váriaveis dentro do array
+
+    //Já o comando pre é pra deixar os arrays mais organizados
+?>
---
+++ Danki-Code/web completo/aula03/criando_variaveis.php
@@ -0,0 +1,7 @@
+<?php
+$nome = 'Marcelo';
+echo 'Meu nome é <br />';
+echo $nome;
+$idade = 21;
+echo 'Meu nome é '.$nome.' e minha idade é '.$idade;
+?>
