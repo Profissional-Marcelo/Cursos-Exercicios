7f4f1734e94e74d91512c7202a57703283d5e30b
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-11-26 22:01:43 -0300

    .

---
+++ Danki-Code/web completo/aula07/if.php
@@ -0,0 +1,23 @@
+<?php
+    $variavel1 = 'Marcelo';
+    $variavel2 = 'Marcelo';
+
+    if($variavel1 == $variavel2){
+       ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>teste</h1>
+        </body>
+        </html>
+        <?php
+    } else if ($variavel1 != $variavel2){
+        
+    }
+
+?>
