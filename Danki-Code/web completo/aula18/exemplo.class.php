b682478f90d63574584f3ca845a91ba4e31fb263
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-12-03 12:13:44 -0300

    .

--- Danki-Code/web completo/aula18/exemplo.class.php
+++ Danki-Code/web completo/aula18/exemplo.class.php
@@ -8,9 +8,20 @@
 
         }
 
+        public static function metodoEstatico(){
+            echo 'metodo estatico';
+        }
+        public static $var3 = 'statico';     
         private function metodo2(){
             
         }
 
+        public function setVar1($var1){
+            $this->var1 = $var1;
+        }
+
+        public function pegaVar1(){
+            return $this->var1;
+        }
     }
 ?>
--- Danki-Code/web completo/aula18/index.php
+++ Danki-Code/web completo/aula18/index.php
@@ -8,4 +8,12 @@
     $exemplo2->var2 = 'Danki Code 2';
 
     echo $exemplo2->var2;
+
+    exemplo::$var3 = 'Outra variavel';
+    echo $exemplo::$var3;
+
+    exemplo::metodoEstatico();
+
+    $exemplo->setVar1('Marcelo');
+    echo $exemplo->pegaVar1()
 ?>
