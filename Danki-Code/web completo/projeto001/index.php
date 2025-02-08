3e344f7a229b41d2bc2a706de9d0ebbf6068d148
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2025-02-07 22:37:38 -0300

    .

---
+++ Danki-Code/web completo/projeto001/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="descrição do meu site">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <title>Projeto 01</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home/a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home/a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>    
+    <section class="banner-principal">
+        <form action="">
+            <h2>Qual o seu melhor e-mail</h2>
+            <input type="email" name="email" required>
+            <input type="submit" value="Cadastrar!" name="acao" >
+        </form>
+    </section>
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Marcelo H C. Rei</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laudantium consectetur, officiis autem sequi sunt placeat nihil non accusantium! Quidem iure quae sapiente dolorem id eligendi eaque voluptatum minima tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure totam reiciendis ad, enim sint assumenda animi quod debitis repellat laborum maiores repudiandae voluptate. Dolor obcaecati iure fuga aliquid. Ducimus, harum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam esse quo, suscipit quisquam culpa nesciunt quos eaque earum amet, hic iusto porro repellat voluptate labore delectus assumenda. Tenetur, dignissimos ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi animi aspernatur minus sint incidunt quae. Ipsum, alias. Et autem similique doloremque blanditiis quam amet perspiciatis? Sit quod aliquam eius quo!</p>
+        </div>
+        <div class="w50">
+            <img src="images/foto.jpeg" alt="">
+        </div>
+    </section>
+</body>
+</html>
