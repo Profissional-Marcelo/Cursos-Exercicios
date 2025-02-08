33c863e3052bb4529f77b944006e0df542867d4e
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2025-02-07 23:56:23 -0300

    projeto danki

--- Danki-Code/web completo/projeto001/index.php
+++ Danki-Code/web completo/projeto001/index.php
@@ -1,48 +1,120 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="descrição do meu site">
     <meta name="keywords" content="palavras-chave,do,meu,site">
+    <link rel="stylesheet" href="estilo/fontawesome.min.css">
+    <link rel="stylesheet" href="estilo/fontawesome.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo/style.css">
     <title>Projeto 01</title>
 </head>
+
 <body>
     <header>
-        <div class="logo">Logomarca</div>
-        <nav class="desktop">
-            <ul>
-                <li><a href="">Home/a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
-        <nav class="mobile">
-            <ul>
-                <li><a href="">Home/a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
-    </header>    
+        <div class="center">
+            <div class="logo">Logomarca</div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
     <section class="banner-principal">
-        <form action="">
-            <h2>Qual o seu melhor e-mail</h2>
-            <input type="email" name="email" required>
-            <input type="submit" value="Cadastrar!" name="acao" >
-        </form>
+        <div class="center">
+            <form action="">
+                <h2>Qual o seu melhor e-mail</h2>
+                <input type="email" name="email" required>
+                <input type="submit" value="Cadastrar!" name="acao">
+            </form>
+        </div>
     </section>
     <section class="descricao-autor">
-        <div class="w50">
-            <h2>Marcelo H C. Rei</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laudantium consectetur, officiis autem sequi sunt placeat nihil non accusantium! Quidem iure quae sapiente dolorem id eligendi eaque voluptatum minima tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure totam reiciendis ad, enim sint assumenda animi quod debitis repellat laborum maiores repudiandae voluptate. Dolor obcaecati iure fuga aliquid. Ducimus, harum.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam esse quo, suscipit quisquam culpa nesciunt quos eaque earum amet, hic iusto porro repellat voluptate labore delectus assumenda. Tenetur, dignissimos ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi animi aspernatur minus sint incidunt quae. Ipsum, alias. Et autem similique doloremque blanditiis quam amet perspiciatis? Sit quod aliquam eius quo!</p>
-        </div>
-        <div class="w50">
-            <img src="images/foto.jpeg" alt="">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Marcelo H C. Rei</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laudantium consectetur, officiis autem sequi sunt placeat nihil non accusantium! Quidem iure quae sapiente dolorem id eligendi eaque voluptatum minima tempore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure totam reiciendis ad, enim sint assumenda animi quod debitis repellat laborum maiores repudiandae voluptate. Dolor obcaecati iure fuga aliquid. Ducimus, harum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam esse quo, suscipit quisquam culpa nesciunt quos eaque earum amet, hic iusto porro repellat voluptate labore delectus assumenda. Tenetur, dignissimos ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi animi aspernatur minus sint incidunt quae. Ipsum, alias. Et autem similique doloremque blanditiis quam amet perspiciatis? Sit quod aliquam eius quo!</p>
+            </div>
+            <div class="w50 left">
+                <img src="images/foto.jpeg" alt="">
+            </div>
         </div>
     </section>
+    <section class="especialidades">
+
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidade">
+                <h3><i class="fa-brands fa-css3"></i></h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci consectetur est accusamus veniam, autem eos mollitia sed esse obcaecati. Fugit optio autem quis repellat repudiandae similique, ut voluptate delectus id!</p>
+            </div>
+            <div class="box-especialidade">
+                <h3><i class="fa-brands fa-html5"></i></h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci consectetur est accusamus veniam, autem eos mollitia sed esse obcaecati. Fugit optio autem quis repellat repudiandae similique, ut voluptate delectus id!</p>
+            </div>
+            <div class="box-especialidade">
+                <h3><i class="fa-solid fa-box"></i></h3>
+                <h3>JavaScript</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci consectetur est accusamus veniam, autem eos mollitia sed esse obcaecati. Fugit optio autem quis repellat repudiandae similique, ut voluptate delectus id!</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est incidunt, sunt veniam maiores harum dolorem modi obcaecati dolorum quia placeat neque animi voluptate vel accusamus libero magni repellendus perferendis eum.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est incidunt, sunt veniam maiores harum dolorem modi obcaecati dolorum quia placeat neque animi voluptate vel accusamus libero magni repellendus perferendis eum.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est incidunt, sunt veniam maiores harum dolorem modi obcaecati dolorum quia placeat neque animi voluptate vel accusamus libero magni repellendus perferendis eum.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div>
+            </div>
+            <div class="w50 leaft">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laborum voluptates odio molestiae? Dolore quam labore tempore, voluptatibus tempora minus distinctio eveniet voluptatem error autem ratione inventore laborum excepturi nostrum?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laborum voluptates odio molestiae? Dolore quam labore tempore, voluptatibus tempora minus distinctio eveniet voluptatem error autem ratione inventore laborum excepturi nostrum?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laborum voluptates odio molestiae? Dolore quam labore tempore, voluptatibus tempora minus distinctio eveniet voluptatem error autem ratione inventore laborum excepturi nostrum?</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
 </body>
+
 </html>
